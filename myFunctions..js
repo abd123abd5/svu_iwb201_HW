cb1d88fc1d24e25d8ae31f18629f9973768eacab
abd123abd5 <129692805+abd123abd5@users.noreply.github.com>
2024-06-25 18:08:40 +0300

    Add files via upload

---
+++ myFunctions..js
@@ -0,0 +1,91 @@
+onload = function(){
+    
+    jQuery(document).ready(function(){
+        jQuery(".one").hide()
+        jQuery(".tow").hide()
+        jQuery(".three").hide()
+        jQuery(".four").hide()
+        jQuery(".five").hide()
+        jQuery(".six").hide()
+        jQuery(".seven").hide()
+        jQuery(".eight").hide()
+        jQuery(".nine").hide()
+        jQuery(".ten").hide()
+        jQuery(".form").hide()
+
+    })
+
+        $(document).ready(function() {
+            $('#a').click(function() {
+                $('.one').slideToggle();
+            });
+        });
+        $(document).ready(function() {
+            $('#b').click(function() {
+                $('.tow').slideToggle();
+            });
+        });
+        $(document).ready(function() {
+            $('#c').click(function() {
+                $('.three').slideToggle();
+            });
+        });
+        $(document).ready(function() {
+            $('#d').click(function() {
+                $('.four').slideToggle();
+            });
+        });
+        $(document).ready(function() {
+            $('#e').click(function() {
+                $('.five').slideToggle();
+            });
+        });
+        $(document).ready(function() {
+            $('#f').click(function() {
+                $('.six').slideToggle();
+            });
+        });
+        $(document).ready(function() {
+            $('#g').click(function() {
+                $('.seven').slideToggle();
+            });
+        });
+        $(document).ready(function() {
+            $('#h').click(function() {
+                $('.eight').slideToggle();
+            });
+        });
+        $(document).ready(function() {
+            $('#i').click(function() {
+                $('.nine').slideToggle();
+            });
+        });
+
+        $(document).ready(function() {
+            $('#k').click(function() {
+                $('.ten').slideToggle();
+            });
+        });
+ // end of hide and show the details//
+
+// 
+ $(document).ready(function() {
+    $('#sub').click(function() {
+        $('.form').slideToggle();
+    });
+ }); 
+ $(document).ready(function() {
+    $('#send').click(function() {
+       alert ("مبروك تم حجز العقار الذي اخترته")
+    });
+ }); 
+ 
+
+
+}
+
+
+   
+
+  
+
